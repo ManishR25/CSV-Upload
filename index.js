5b50e7f65d81715ad34c6a6619ea5b87ec3bda9e
Manish Rane <80664486+ManishR25@users.noreply.github.com>
2023-10-28 12:45:02 +0530

    Add files via upload
    FIle has been commited

---
+++ index.js
@@ -0,0 +1,25 @@
+import "./env.js";
+import express from 'express';
+import path from 'path';
+import ejsLayouts from 'express-ejs-layouts';
+import csvRouter from './src/routes/csv.routes.js'
+import {connectToDB} from './src/config/mongooseConfig.js';
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(ejsLayouts);
+
+//ejs
+app.set('view engine', 'ejs');
+app.set('views',path.join(path.resolve(), 'src', 'views'));
+//routes
+app.use("/", csvRouter);
+
+//server listening
+app.listen(3000, (err) => {
+    if(err) console.log("server error on port 3000");
+    console.log('Server is running at 3000');
+    connectToDB();
+})
